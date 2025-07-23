a732282e354e64593a977843faa5124a1e2d3767
Aishwarya JR <165312078+Aishwarya-jr2003@users.noreply.github.com>
2025-07-23 14:02:19 +0000

    varied

--- DP/1D_DP/frogjump_two.cpp
+++ DP/1D_DP/frogjump_two.cpp
@@ -1,14 +1,10 @@
-Given an integer array nums of size n.
-Return the maximum sum possible using the elements 
-of nums such that no two elements taken are adjacent in nums.
+A frog wants to climb a staircase with n steps. Given an integer array heights,
+ where heights[i] contains the height of the ith step, and an integer k.
 
-
-Examples:
-Input: nums = [1, 2, 4]
-
-Output: 5
-
-Explanation: [1, 2, 4], the underlined elements are taken to get the maximum sum.
+To jump from the ith step to the jth step, the frog requires abs(heights[i] - heights[j])
+ energy, where abs() denotes the absolute difference. The frog can jump 
+ from the ith step to any step in the range [i + 1, i + k], provided it exists. 
+ Return the minimum amount of energy required by the frog to go from the 0th step to the (n-1)th step.
 
 
 
