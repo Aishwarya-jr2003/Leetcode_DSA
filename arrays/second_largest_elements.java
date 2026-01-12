d49c40b8483e9917f5adb9a32b68936acb837026
Aishwarya <jraishwarya21@gmail.com>
2026-01-12 17:20:00 +0530

    mmm

--- arrays/second_largest_elements.java
+++ arrays/second_largest_elements.java
@@ -1,5 +1,25 @@
 package arrays;
 
 public class second_largest_elements {
-    
+      public int secondLargestElement(int[] nums) {
+       
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargestElement = Integer.MIN_VALUE;
+        boolean foundSecond = false;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]> largest){
+                secondLargestElement = largest;
+                largest = nums[i];
+            }
+            else if(nums[i]<largest){
+                secondLargestElement = Math.max(secondLargestElement,nums[i]);
+            }
+        }
+       if (secondLargestElement == Integer.MIN_VALUE)
+          return -1;
+          return secondLargestElement;
+        // edge case for the larg = Integer.MIN_VALUE+1 and sl = Integer.MIN_VALUE;
+    }
+
 }
